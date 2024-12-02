109ff27836915b2f5dee1582812f5ded6e9ca0ea
Ashutosh-at-079925877420 <ashutosh.d@girdmail.com>
2024-12-02 14:09:03 +0530

    .

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:get/get.dart';
 import 'package:out_duty/Apply/ApplyOutDuty.dart';
 import 'package:leave/apply/ApplyLeave.dart';
